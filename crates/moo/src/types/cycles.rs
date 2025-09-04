997735defee7bff4c5372ddef68a0aee938fc98b
dbalsom <dbalsom@gmail.com>
2025-09-04 06:35:49 -0400

    implement MooCycleStatePrinter

--- crates/moo/src/types/cycles.rs
+++ crates/moo/src/types/cycles.rs
@@ -1,5 +1,6 @@
 use binrw::binrw;
 use std::fmt::Display;
+use crate::types::{MooBusState, MooCpuType, MooCpuWidth, MooDataWidth, MooTState};
 
 #[derive(Copy, Clone, Debug, Default)]
 #[binrw]
@@ -18,37 +19,210 @@ pub struct MooCycleState {
     pub queue_byte: u8,
 }
 
-impl Display for MooCycleState {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let ale_str = if self.pins0 & MooCycleState::PIN_ALE != 0 {
-            "A:"
-        } else {
-            "  "
-        };
-
-        let r_chr = if self.memory_status & 0b100 != 0 {
-            "R"
-        } else {
-            "."
-        };
-
-        let w_chr = if self.memory_status & 0b001 != 0 {
-            "W"
-        } else {
-            "."
-        };
-
-        write!(
-            f,
-            "{}{:06X}:{:04X} m:{}.{} i:... ",
-            ale_str, self.address_bus, self.data_bus, r_chr, w_chr,
-        )
-    }
-}
-
 impl MooCycleState {
     pub const PIN_ALE: u8 = 0b0000_0001;
     pub const PIN_BHE: u8 = 0b0000_0010;
     pub const PIN_READY: u8 = 0b0000_0100;
     pub const PIN_LOCK: u8 = 0b0000_1000;
+
+    pub const MRDC_BIT: u8 = 0b0000_0001;
+    pub const AMWC_BIT: u8 = 0b0000_0010;
+    pub const MWTC_BIT: u8 = 0b0000_0100;
+
+    pub const IORC_BIT: u8 =  0b0000_0001;
+    pub const AIOWC_BIT: u8 = 0b0000_0010;
+    pub const IOWC_BIT: u8 = 0b0000_0100;
+
+    #[inline]
+    pub fn bhe(&self) -> bool {
+        self.pins0 & MooCycleState::PIN_BHE == 0
+    }
+    #[inline]
+    pub fn ale(&self) -> bool {
+        self.pins0 & MooCycleState::PIN_ALE != 0
+    }
+    #[inline]
+    pub fn t_state(&self) -> MooTState {
+        MooTState::try_from(self.t_state & 0x07).unwrap_or(MooTState::Ti)
+    }
+    #[inline]
+    pub fn is_reading_mem(&self) -> bool {
+        (self.memory_status & Self::MRDC_BIT) == 0
+    }
+    #[inline]
+    pub fn is_writing_mem(&self) -> bool {
+        (self.memory_status & Self::MWTC_BIT) == 0
+    }
+    #[inline]
+    pub fn is_reading_io(&self) -> bool {
+        (self.io_status & Self::IORC_BIT) == 0
+    }
+    #[inline]
+    pub fn is_writing_io(&self) -> bool {
+        (self.io_status & Self::IOWC_BIT) == 0
+    }
+    #[inline]
+    pub fn is_reading(&self) -> bool {
+        self.is_reading_mem() || self.is_reading_io()
+    }
+    #[inline]
+    pub fn is_writing(&self) -> bool {
+        self.is_writing_mem() || self.is_writing_io()
+    }
+}
+
+pub struct MooCycleStatePrinter {
+    pub cpu_type: MooCpuType,
+    pub address_latch: u32,
+    pub state: MooCycleState,
+}
+
+impl MooCycleStatePrinter {
+    pub fn data_width(&self) -> MooDataWidth {
+        let cpu_width = MooCpuWidth::from(self.cpu_type);
+        match cpu_width {
+            MooCpuWidth::Eight => MooDataWidth::EightLow,
+            MooCpuWidth::Sixteen => {
+                if (self.address_latch & 1 != 0)
+                    && (self.state.pins0 & MooCycleState::PIN_ALE == 0)
+                {
+                    MooDataWidth::EightHigh
+                }
+                else if self.state.pins0 & MooCycleState::PIN_BHE == 0 {
+                    MooDataWidth::Sixteen
+                }
+                else {
+                    MooDataWidth::EightLow
+                }
+            }
+        }
+    }
+
+    pub fn data_bus_str(&self) -> String {
+        match self.data_width() {
+            MooDataWidth::Invalid => "----".to_string(),
+            MooDataWidth::Sixteen => format!("{:04X}", self.state.data_bus),
+            MooDataWidth::EightLow => format!("{:>4}", format!("{:02X}", self.state.data_bus as u8)),
+            MooDataWidth::EightHigh => format!("{:<4}", format!("{:02X}", (self.state.data_bus >> 8) as u8)),
+        }
+    }
+}
+
+impl Display for MooCycleStatePrinter {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let ale_str = if self.state.pins0 & MooCycleState::PIN_ALE != 0 {
+            "A:"
+        } else {
+            "  "
+        };
+
+        let mut seg_str = "  ".to_string();
+
+        let rs_chr = match self.state.memory_status & MooCycleState::MRDC_BIT != 0 {
+            true => "R",
+            false => ".",
+        };
+        let aws_chr = match self.state.memory_status & MooCycleState::AMWC_BIT == 0 {
+            true => 'A',
+            false => '.',
+        };
+        let ws_chr = match self.state.memory_status & MooCycleState::MWTC_BIT != 0 {
+            true => "W",
+            false => ".",
+        };
+        let ior_chr = match self.state.io_status & MooCycleState::IORC_BIT == 0 {
+            true => 'R',
+            false => '.',
+        };
+        let aiow_chr = match self.state.io_status & MooCycleState::AIOWC_BIT == 0 {
+            true => 'A',
+            false => '.',
+        };
+        let iow_chr = match self.state.io_status & MooCycleState::IOWC_BIT == 0 {
+            true => 'W',
+            false => '.',
+        };
+
+        let bhe_chr = match self.state.bhe() {
+            true => 'B',
+            false => '.',
+        };
+
+        let intr_chr = '.';
+        let inta_chr = '.';
+
+        let bus_state = self.cpu_type.decode_status(self.state.bus_state);
+        let bus_raw = self.cpu_type.raw_status(self.state.bus_state);
+        let bus_str = bus_state.to_string();
+
+        let t_state = self.state.t_state.try_into().unwrap_or(MooTState::Ti);
+        let t_string = self.cpu_type.tstate_to_string(t_state);
+
+        let mut xfer_str = "        ".to_string();
+
+
+        let bus_active = match self.cpu_type {
+            MooCpuType::Intel80386Ex => {
+                // The 386 can write on t1
+                if self.state.is_writing() {
+                    true
+                }
+                else {
+                    // The 386 can read after T1
+                    t_state != MooTState::T1
+                }
+            }
+            MooCpuType::Intel80286 => {
+                // The 286 can read/write after T1
+                t_state != MooTState::T1
+            }
+            _ => {
+                // Older CPUs can only read/write in PASV state
+                bus_state == MooBusState::PASV
+            }
+        };
+
+        if bus_active {
+            let value = self.data_bus_str();
+            if self.state.is_reading() {
+                xfer_str = format!("r-> {}", value);
+            }
+            else if self.state.is_writing() {
+                xfer_str = format!("<-w {}", value);
+            }
+        }
+
+        let bus_chr_width = self.cpu_type.bus_chr_width();
+        let data_chr_width = self.cpu_type.data_chr_width();
+
+        write!(
+            f,
+            "{ale_str:02}{addr_latch:0bus_chr_width$X}:{addr_bus:0bus_chr_width$X}:{data_bus:0data_chr_width$X} \
+            {seg_str:02} M:{rs_chr}{aws_chr}{ws_chr} I:{ior_chr}{aiow_chr}{iow_chr} \
+            P:{intr_chr}{inta_chr}{bhe_chr} {bus_str:04}[{bus_raw:01}] {t_str:02} {xfer_str:06}",
+            ale_str = ale_str,
+            addr_latch = self.address_latch,
+            addr_bus = self.state.address_bus,
+            data_bus = self.state.data_bus,
+            bus_chr_width = bus_chr_width,
+            seg_str = seg_str,
+            rs_chr = rs_chr,
+            aws_chr = aws_chr,
+            ws_chr = ws_chr,
+            ior_chr = ior_chr,
+            aiow_chr = aiow_chr,
+            iow_chr = iow_chr,
+            intr_chr = intr_chr,
+            inta_chr = inta_chr,
+            bhe_chr = bhe_chr,
+            bus_str = bus_str,
+            bus_raw = bus_raw,
+            t_str = t_string,
+            xfer_str = xfer_str,
+            // q_op_chr = q_op_chr,
+            // q_str = self.queue.to_string(),
+            // width = self.queue.size() * 2,
+            // q_read_str = q_read_str,
+        )
+    }
 }
--- crates/moo/src/types/mod.rs
+++ crates/moo/src/types/mod.rs
@@ -9,6 +9,7 @@ pub mod registers_32;
 pub mod state;
 pub mod test;
 
+use std::fmt::Display;
 use binrw::binrw;
 pub use cycles::*;
 pub use metadata::*;
@@ -19,6 +20,7 @@ pub use registers_32::*;
 pub use state::*;
 pub use test::*;
 
+/// [MooCpuType] represents the type of CPU used to produce a test case.
 #[derive(Copy, Clone, Debug, Default)]
 #[cfg_attr(feature = "serde", derive(serde::Deserialize))]
 #[binrw]
@@ -43,6 +45,66 @@ pub enum MooStateType {
     Final,
 }
 
+/// [MooCpuWidth] represents the native bus size of a CPU.
+#[derive(Copy, Clone, Debug, Default)]
+pub enum MooCpuWidth {
+    #[default]
+    Eight,
+    Sixteen,
+}
+
+impl From<MooCpuType> for MooCpuWidth {
+    fn from(cpu_type: MooCpuType) -> Self {
+        match cpu_type {
+            MooCpuType::Intel8088 | MooCpuType::NecV20 | MooCpuType::Intel80188 => {
+                MooCpuWidth::Eight
+            }
+            _ => MooCpuWidth::Sixteen,
+        }
+    }
+}
+
+/// [MooDataWidth] represents the current width of the data bus.
+#[derive(Copy, Clone, Debug, Default)]
+pub enum MooDataWidth {
+    #[default]
+    Invalid,
+    /// The entire data bus is being driven.
+    Sixteen,
+    /// The low half of the data bus is being driven, A0 is even.
+    EightLow,
+    /// The low half of the data bus is being driven, A0 is odd.
+    EightHigh,
+}
+
+#[derive(Copy, Clone, Debug, PartialEq)]
+pub enum MooBusState {
+    INTA = 0, // IRQ Acknowledge
+    IOR = 1,  // IO Read
+    IOW = 2,  // IO Write
+    HALT = 3, // Halt
+    CODE = 4, // Code
+    MEMR = 5, // Memory Read
+    MEMW = 6, // Memory Write
+    PASV = 7, // Passive
+}
+
+impl Display for MooBusState {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        use MooBusState::*;
+        match self {
+            INTA => write!(f, "INTA"),
+            IOR => write!(f, "IOR "),
+            IOW => write!(f, "IOW "),
+            HALT => write!(f, "HALT"),
+            CODE => write!(f, "CODE"),
+            MEMR => write!(f, "MEMR"),
+            MEMW => write!(f, "MEMW"),
+            PASV => write!(f, "PASV"),
+        }
+    }
+}
+
 pub enum MooIvtOrder {
     ReadFirst,
     PushFirst,
@@ -57,8 +119,52 @@ impl From<MooCpuType> for MooIvtOrder {
     }
 }
 
+#[derive(Copy, Clone, PartialEq)]
+pub enum MooTState {
+    Ti,
+    T1,
+    T2,
+    T3,
+    T4,
+    Tw,
+}
+
+impl TryFrom<u8> for MooTState {
+    type Error = String;
+
+    fn try_from(value: u8) -> Result<Self, Self::Error> {
+        match value {
+            0 => Ok(MooTState::Ti),
+            1 => Ok(MooTState::T1),
+            2 => Ok(MooTState::T2),
+            3 => Ok(MooTState::T3),
+            4 => Ok(MooTState::T4),
+            5 => Ok(MooTState::Tw),
+            _ => Err(format!("Invalid bus cycle value: {}", value)),
+        }
+    }
+}
+
 impl MooCpuType {
 
+    pub fn bus_chr_width(&self) -> usize {
+        use MooCpuType::*;
+        match self {
+            Intel80286 => 6,
+            Intel80386Ex => 6,
+            _ => 5,
+        }
+    }
+
+    pub fn data_chr_width(&self) -> usize {
+        use MooCpuType::*;
+        match self {
+            Intel8088 | NecV20 => 2,
+            _ => 4,
+        }
+    }
+
+    /// Convert a string representation of a CPU type to a [MooCpuType].
     pub fn from_str(str: &str) -> Result<MooCpuType, String> {
         if (str == "286 ") || (str == "C286") {
             Ok(MooCpuType::Intel80286)
@@ -81,6 +187,83 @@ impl MooCpuType {
         }
     }
 
+    pub fn tstate_to_string(&self, state: MooTState) -> String {
+        use MooTState::*;
+        match self {
+            MooCpuType::Intel80286 => match state {
+                Ti => "Ti".to_string(),
+                T1 => "Ts".to_string(),
+                T2 => "Tc".to_string(),
+                Tw => "Tw".to_string(),
+                _ => "T?".to_string(),
+            },
+            _ => match state {
+                Ti => "Ti".to_string(),
+                T1 => "T1".to_string(),
+                T2 => "T2".to_string(),
+                T3 => "T3".to_string(),
+                T4 => "T4".to_string(),
+                Tw => "Tw".to_string(),
+            },
+        }
+    }
+
+    pub fn decode_status(&self, status_byte: u8) -> MooBusState {
+        use MooCpuType::*;
+        use MooBusState::*;
+        match self {
+            Intel80286 => match status_byte & 0x0F {
+                0b0000 => INTA,
+                0b0001 => PASV, // Reserved
+                0b0010 => PASV, // Reserved
+                0b0011 => PASV, // None
+                0b0100 => HALT,
+                0b0101 => MEMR,
+                0b0110 => MEMW,
+                0b0111 => PASV, // None
+                0b1000 => PASV, // Reserved
+                0b1001 => IOR,
+                0b1010 => IOW,
+                0b1011 => PASV, // None
+                0b1100 => PASV, // Reserved
+                0b1101 => CODE,
+                0b1110 => PASV, // Reserved
+                0b1111 => PASV, // None
+                _ => PASV,      // Default to passive state
+            },
+            Intel80386Ex => match status_byte & 0x07 {
+                0 => INTA, // IRQ Acknowledge
+                1 => PASV, // Passive
+                2 => IOR,  // IO Read
+                3 => IOW,  // IO Write
+                4 => CODE, // Code fetch
+                5 => HALT, // Halt
+                6 => MEMR, // Memory Read
+                _ => MEMW, // Memory Write
+            },
+            _ => {
+                match status_byte & 0x07 {
+                    0 => INTA, // IRQ Acknowledge
+                    1 => IOR,  // IO Read
+                    2 => IOW,  // IO Write
+                    3 => HALT, // Halt
+                    4 => CODE, // Code fetch
+                    5 => MEMR, // Memory Read
+                    6 => MEMW, // Memory Write
+                    _ => PASV, // Passive state
+                }
+            }
+        }
+    }
+
+    pub fn raw_status(&self, status_byte: u8) -> u8 {
+        match self {
+            MooCpuType::Intel80286 => status_byte & 0x0F,
+            _ => status_byte & 0x07,
+        }
+    }
+
+    /// Return the numeric bit width of the CPU data bus (8 or 16).
     pub fn bus_bitness(&self) -> u32 {
         if self.has_16bit_bus() {
             16
@@ -89,6 +272,7 @@ impl MooCpuType {
         }
     }
 
+    /// Return the numeric bit width of the CPU registers (16 or 32).
     pub fn reg_bitness(&self) -> u32 {
         if self.has_32bit_regs() {
             32
@@ -97,10 +281,12 @@ impl MooCpuType {
         }
     }
 
+    /// Return true if the CPU has 32-bit registers.
     pub fn has_32bit_regs(&self) -> bool {
         matches!(self, MooCpuType::Intel80386Ex)
     }
 
+    /// Return true if the CPU has a native 16-bit data bus.
     pub fn has_16bit_bus(&self) -> bool {
         matches!(
             self,
@@ -112,6 +298,7 @@ impl MooCpuType {
         )
     }
 
+    /// Return true if the CPU has a native 8-bit data bus.
     pub fn has_8bit_bus(&self) -> bool {
         matches!(
             self,
@@ -119,6 +306,7 @@ impl MooCpuType {
         )
     }
 
+    /// Return true if the CPU is an Intel CPU (or authorized 2nd source).
     pub fn is_intel(&self) -> bool {
         matches!(
             self,
@@ -131,6 +319,7 @@ impl MooCpuType {
         )
     }
 
+    /// Return true if the CPU is an NEC V20 or V30.
     pub fn is_nec(&self) -> bool {
         matches!(self, MooCpuType::NecV20 | MooCpuType::NecV30)
     }
